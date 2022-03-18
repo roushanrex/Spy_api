ee285a5dfb9673d9e8e159d593af3a45560d6f9c
Roushan Alam <aroushan9@gmail.com>
2022-03-18 22:55:01 +0530

    Update urls.py

--- spyproject/urls.py
+++ spyproject/urls.py
@@ -1,9 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('spyapi.urls')),
+    path('', include('api.urls')),
 ]
