b45eb909c2155833ed90909013b91eb1c3b956b0
Roushan Alam <aroushan9@gmail.com>
2022-03-18 22:50:27 +0530

    Update admin.py

--- spyapi/admin.py
+++ spyapi/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from . models import User,Phonedata
+from . models import User,Phonedata,Audio,Image
 
+#
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Phonedata)
-
+admin.site.register(Audio)
+admin.site.register(Image)
